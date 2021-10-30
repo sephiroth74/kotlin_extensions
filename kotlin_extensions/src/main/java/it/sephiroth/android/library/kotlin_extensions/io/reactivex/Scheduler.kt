efc20020e1e79dab118f8f08474c0874350b96aa
Alessandro Crugnola <alessandro.crugnola@gmail.com>
2021-10-31 01:02:25 +0200

    merged from development

--- kotlin_extensions/src/main/java/it/sephiroth/android/library/kotlin_extensions/io/reactivex/Scheduler.kt
+++ kotlin_extensions/src/main/java/it/sephiroth/android/library/kotlin_extensions/io/reactivex/Scheduler.kt
@@ -1,8 +1,6 @@
 package it.sephiroth.android.library.kotlin_extensions.io.reactivex
 
-import io.reactivex.Observable
-import io.reactivex.Scheduler
-import io.reactivex.Single
+import io.reactivex.*
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
@@ -13,6 +11,30 @@ fun <T> rxSingle(thread: Scheduler, func: () -> T): Single<T> {
     return Single.create<T> { emitter ->
         try {
             emitter.onSuccess(func.invoke())
+        } catch (error: Throwable) {
+            emitter.onError(error)
+        }
+    }.subscribeOn(thread)
+}
+
+fun <T> rxMaybe(thread: Scheduler, func: () -> T?): Maybe<T> {
+    return Maybe.create<T> { emitter ->
+        try {
+            val result = func.invoke()
+            if (!emitter.isDisposed) {
+                result?.let { emitter.onSuccess(result) } ?: run { emitter.onComplete() }
+            }
+        } catch (error: Throwable) {
+            if (!emitter.isDisposed) emitter.onError(error)
+        }
+    }.subscribeOn(thread)
+}
+
+fun rxCompletable(thread: Scheduler, func: () -> Unit): Completable {
+    return Completable.create { emitter ->
+        try {
+            func.invoke()
+            emitter.onComplete()
         } catch (error: Throwable) {
             emitter.onError(error)
         }
@@ -40,7 +62,7 @@ fun doOnScheduler(scheduler: Scheduler, func: () -> Unit): Disposable {
 }
 
 fun doOnMainThread(func: () -> Unit): Disposable? {
-    if(isMainThread()) {
+    if (isMainThread()) {
         func.invoke()
         return null
     }
