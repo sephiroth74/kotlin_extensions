983965aa677d0ea908577063df27a7c8e58ca659
Alessandro Crugnola <alessandro.crugnola@gmail.com>
2021-11-27 13:45:22 +0100

    packaging

--- kotlin_extensions/src/main/java/it/sephiroth/android/library/kotlin_extensions/os/build/Build.kt
+++ kotlin_extensions/src/main/java/it/sephiroth/android/library/kotlin_extensions/os/build/Build.kt
@@ -2,6 +2,6 @@ package it.sephiroth.android.library.kotlin_extensions.os.build
 
 import android.os.Build
 
-fun Build.isAPI(value: Int) = Build.VERSION.SDK_INT == value
+fun isAPI(value: Int) = Build.VERSION.SDK_INT == value
 
-fun Build.isAtLeastAPI(value: Int) = Build.VERSION.SDK_INT >= value
+fun isAtLeastAPI(value: Int) = Build.VERSION.SDK_INT >= value
