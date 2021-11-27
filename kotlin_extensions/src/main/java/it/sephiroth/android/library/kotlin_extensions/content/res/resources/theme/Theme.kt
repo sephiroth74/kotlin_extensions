144bb7a4a7b20a981e6e499ae21119b822e8d569
Alessandro Crugnola <alessandro.crugnola@gmail.com>
2021-11-27 13:35:05 +0100

    theme utils

--- kotlin_extensions/src/main/java/it/sephiroth/android/library/kotlin_extensions/content/res/resources/theme/Theme.kt
+++ kotlin_extensions/src/main/java/it/sephiroth/android/library/kotlin_extensions/content/res/resources/theme/Theme.kt
@@ -3,8 +3,11 @@ package it.sephiroth.android.library.kotlin_extensions.content.res.resources.the
 import android.content.Context
 import android.content.res.ColorStateList
 import android.content.res.Resources.Theme
+import android.graphics.drawable.Drawable
 import android.util.TypedValue
+import androidx.annotation.AnyRes
 import androidx.annotation.AttrRes
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.content.ContextCompat
 
 
@@ -34,4 +37,12 @@ fun Theme.getColorStateList(context: Context, @AttrRes id: Int): ColorStateList?
 
 fun Theme.getColor(context: Context, @AttrRes id: Int): Int {
     return ContextCompat.getColor(context, context.theme.resolveAttribute(id))
+}
+
+fun Theme.getDrawable(context: Context, @AttrRes id: Int): Drawable? {
+    return AppCompatResources.getDrawable(context, context.theme.getDrawableRes(context, id))
+}
+
+fun Theme.getDrawableRes(context: Context, @AttrRes id: Int): Int {
+    return context.theme.getTypedValue(id).resourceId
 }
